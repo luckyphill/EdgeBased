89877a7994c59a742d25d5976e18ada758fa58d6
Phillip Brown <pbrown.mwerhun@gmail.com>
2021-05-13 17:50:05 +0930

    Testing some tensions

--- HPC/MembranePressureAnalysis/launcher.sh
+++ HPC/MembranePressureAnalysis/launcher.sh
@@ -1,1 +1,1 @@
-sbatch --array=0-16 --time=24:00:00 ../generalSbatch6seed.sh ManageTumourInMembrane MembranePressureAnalysis.txt 
+sbatch --array=0-10 --time=24:00:00 ../generalSbatch6seed.sh ManageTumourInMembrane MembranePressureAnalysis.txt 
--- analysis/Framework Paper Analyses/MembranePressureAnalysis.m
+++ analysis/Framework Paper Analyses/MembranePressureAnalysis.m
@@ -7,11 +7,11 @@ classdef MembranePressureAnalysis < Analysis
 		t0 = 9;
 		tg = 9;
 
-		mpe = 100;
+		mpe = 10:10:100;
 
 		f = 0.9;
 
-		dF = round(logspace(-1,2,16),2,'significant'); % Log steps from 0.1 to 100
+		dF = 0.5; % Log steps from 0.1 to 100
 
 		seed = 1;
 
